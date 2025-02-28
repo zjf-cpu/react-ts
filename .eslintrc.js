752a0e937802bf24c675463a59337ee7163de294
zjf-cpu <1137183005@qq.com>
2025-02-28 17:01:38 +0800

    ff

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,25 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   overrides: [],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersiion: "latest",
-    sourceType: "module",
+    ecmaVersiion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   rules: {},
-};
+  // ...其他配置
+  settings: {
+    react: {
+      version: 'detect', // 自动检测 package.json 中的 React 版本
+      // 或手动指定版本：
+      // version: "18.2.0"  // 替换为你的实际版本
+    },
+  },
+}
